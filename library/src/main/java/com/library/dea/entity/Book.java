e18346ba0eb135e3dfb06ce3a7ab4b81bcb78f92
atakishiyevahalima-byte <atakishiyevahalima@gmail.com>
2026-01-07 21:10:30 +0400

    Mapping

---
+++ library/src/main/java/com/library/dea/entity/Book.java
@@ -0,0 +1,46 @@
+package com.library.dea.entity;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+   private String title;
+   private String author;
+
+   Book(){
+
+   }
+
+   Book(String title, String author){
+       this.title = title;
+       this.author = author;
+   }
+   public Integer getId(){
+       return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
