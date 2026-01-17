50ab8ac716f34d75e944f911a6f46f338fa6b800
atakishiyevahalima-byte <atakishiyevahalima@gmail.com>
2026-01-17 19:04:09 +0400

    Query, native query methods

--- library/src/main/java/com/library/dea/controller/BookController.java
+++ library/src/main/java/com/library/dea/controller/BookController.java
@@ -21,6 +21,23 @@ public class BookController {
         return bookService.showAll();
     }
 
+    @GetMapping("/find/title/{title}")
+    public List<Book> getAllBooksByTitle(@PathVariable String title){
+        return bookService.getAllByTitle(title);
+    }
+    @GetMapping("/find/author/{author}")
+    public List<Book> getAllBooksByAuthor(@PathVariable String author){
+        return bookService.getAllByAuthor(author);
+    }
+    @GetMapping("/find/price/{minPrice}")
+    public List<Book> getAllByMinPrice(@PathVariable Double minPrice){
+        return bookService.getAllByMinPrice(minPrice);
+    }
+    @GetMapping("/find/amount/{minAmount}")
+    public List<Book> getAllByMinAmount(@PathVariable Integer minAmount){
+        return bookService.getAllByMinAmount(minAmount);
+    }
+
     @GetMapping("/{id}")
     public Book getBook(@PathVariable Integer id){
         return bookService.showById(id);
